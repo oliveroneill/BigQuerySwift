17287b16425434152f32788535e462d3ee724295
Oliver <oliveroneill04@gmail.com>
2018-11-07 08:40:05 +1100

    Fix HTTP client no longer errors with unsuccessful responses

--- Sources/BigQuerySwift/HTTPClient.swift
+++ Sources/BigQuerySwift/HTTPClient.swift
@@ -14,10 +14,18 @@ public class SwiftyRequestClient: HTTPClient {
     public func post(url: String, payload: Data,
               headers: [String:String],
               completionHandler: @escaping (Data?, HTTPURLResponse?, Error?) -> Void) {
-        let request = RestRequest(method: .get, url: url)
+        let request = RestRequest(method: .post, url: url)
         request.messageBody = payload
         request.headerParameters = headers
         request.contentType = "application/json"
-        request.response(completionHandler: completionHandler)
+        request.response() { body, response, error in
+            // We don't want bad responses coming through as errors since the
+            // body could still be valid
+            if let _ = error as? RestError {
+                completionHandler(body, response, nil)
+            } else {
+                completionHandler(body, response, error)
+            }
+        }
     }
 }
